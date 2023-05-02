396e2adbb779d4e020bda983e99732eb439b3488
Timiemmy <faith.bolanle@yahoo.com>
2023-05-02 15:19:20 +0100

    Updated models and add new content class

--- courses/models.py
+++ courses/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.fields import GenericForeignKey
 
 class Subject(models.Model):
     title = models.CharField(max_length=200)
@@ -34,3 +36,36 @@ class Module(models.Model):
 
     def __str__(self):
         return self.title
+    
+
+class Content(models.Model):
+    module = models.ForeignKey(Module, related_name='contents', on_delete=models.CASCADE)
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE) # A ForeignKey field to the ContentType model.
+    object_id = models.PositiveIntegerField()# A PositiveIntegerField to store the primary key of the related object.
+    item = GenericForeignKey('content_type', 'object_id') # A GenericForeignKey field to the related object combining the two previous fields.
+
+
+class ItemBase(models.Model):
+    owner = models.ForeignKey(User, related_name='%(class)s_related', on_delete=models.CASCADE)
+    title = models.CharField(max_length=250)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+    def __str__(self):
+        return self.title
+    
+
+class Text(ItemBase):
+    content = models.TextField()
+
+class File(ItemBase):
+    file = models.FileField(upload_to='files')
+
+class Image(ItemBase):
+    file = models.FileField(upload_to='images')
+
+class Video(ItemBase):
+    url = models.URLField()
